74172e28df121669ab41dc20c93996ec96c7f864
Laura Alarcón <lxalarcon0@misena.edu.co>
2023-05-09 00:41:58 -0500

    modificacion de registrar

--- src/app/service/serviceuser.service.ts
+++ src/app/service/serviceuser.service.ts
@@ -15,7 +15,12 @@ export class ServiceuserService {
   constructor(private http:HttpClient) { }
 
   getAllUser():Observable<ResponseI>{
-   return this.http.get<ResponseI>(urlBase +"/all")
+   return this.http.get<ResponseI>(urlBase +"/all");
     //return this.http.get<ResponseI>("http://localhost:8083/usuario/all");
   }
-}
+   crearUsuario(data:any):Observable<ResponseI>{
+    return this.http.post<ResponseI>(urlBase+"/create",data);
+   }
+
+  }
+
--- src/app/template/usuario/form-usuario/form-component.component.ts
+++ src/app/template/usuario/form-usuario/form-component.component.ts
@@ -1,8 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ServiceuserService } from 'src/app/service/serviceuser.service';
 
 @Component({
   selector: 'app-form-component',
   templateUrl: './form-component.component.html',
   styleUrls: ['./form-component.component.css']
 })
-export class FormComponentComponent {}
+export class FormComponentComponent implements OnInit{
+
+  usuarioForm=new FormGroup({
+    id:new FormControl(''),
+    nombre:new FormControl (''),
+    apellido:new FormControl(''),
+    correo:new FormControl(''),
+    documento: new FormControl(''),
+    direccion:new FormControl(''),
+    fechaNac:new FormControl(''),
+    edad:new FormControl(''),
+    telefono:new FormControl(''),
+    fechaIngreso:new FormControl('')
+
+});
+  constructor(private user:ServiceuserService){}
+  ngOnInit(): void {
+
+  }
+  addUsuario():void{
+    let usuario={
+      id:this.usuarioForm.get('id')?.value,
+      nombre:this.usuarioForm.get('nombre')?.value,
+      apellido:this.usuarioForm.get('apellido')?.value,
+      correo:this.usuarioForm.get('correo')?.value,
+      documento:this.usuarioForm.get('documento')?.value,
+      direccion:this.usuarioForm.get('diereccion')?.value,
+      fechaNac:this.usuarioForm.get('fechaNac')?.value,
+      edad:this.usuarioForm.get('edad')?.value,
+      telefono:this.usuarioForm.get('telefono')?.value,
+      fechaingreso:this.usuarioForm.get('fechaIngreso')?.value
+
+    }
+      this.user.crearUsuario(usuario).subscribe(res=>{
+        if(res.status==="ok"){
+          alert("Registro exitoso")
+        }
+      });
+  }
+
+}
